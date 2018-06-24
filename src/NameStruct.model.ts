82bdb84b07b1202510ccf2361928211b9ac4e0e8
Jonathan Wells <jdewells25@gmail.com>
2018-06-24 16:46:43 +0100

    Late start at ~15:20 after lunch. Major refactor to NameCounter to implement the full monty of required stuff. Didn't TDD for this part since it was more of a jot-down of the way it was going to work; now implementing tests to iron out any kinks and get the motor running properly.

--- src/NameStruct.model.ts
+++
@@ -1,8 +0,0 @@
-export default class NameStruct {
-    // TODO: Is a constructor appropriate here?
-    constructor(
-        public titles: Array<string>,
-        public firstNames: Array<string>,
-        public lastNames: Array<string>
-    ) {};
-};
